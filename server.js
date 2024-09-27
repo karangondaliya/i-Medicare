224971e8086fb97ca38875bdb3cf73d4ea9f3e52
Karan Gondaliya <139796588+karangondaliya@users.noreply.github.com>
2024-09-27 16:21:08 +0530

    Deployment Code

--- server.js
+++ server.js
@@ -1,55 +1,45 @@
 const express = require('express');
 const colors = require('colors');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const fileUpload = require('express-fileupload');
 const path = require('path');
 const cors = require('cors');
 
-//dotenv config
-dotenv.config()
+// dotenv config
+dotenv.config();
 
-//MongoDB Connection
+// MongoDB Connection
 connectDB();
 
-//REST Object
+// REST Object
 const app = express();
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
-//middlewares
+// middlewares
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(fileUpload());
 app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  }));
-  
+}));
 
-//Static Files
+// Static Files
 app.use(express.static(path.join(__dirname, './client/build')));
 
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
-})
-
-//routes
+// API routes
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/v1/user', require('./routes/user-routes'));
 app.use('/api/v1/admin', require('./routes/admin-routes'));
 app.use('/api/v1/doctor', require('./routes/doctor-routes'));
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(
-        app.listen(port, () => {
-            // console.log(`Server Running in ${process.env.NODE_MODE} Mode on Port ${port}
-            // http://localhost:8080/`);
-        })
-    )
-    .catch((err) => {
-        console.log(err);
-    });
+// Serve React app
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+});
 
-
-//listen port
+// Listen on the specified port
+app.listen(port, () => {
+    //console.log(`Server running on port ${port}`)
+})
